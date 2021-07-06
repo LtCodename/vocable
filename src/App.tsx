aaabc922ff2763b0d2c26938d3220c5b6a34779a
Yevhen Chernenko <chernenko.eu@gmail.com>
2021-07-06 16:33:33 +0300

    router + styled comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <div>Vocable</div>;
-}
+import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
+import Main from "./components/Main/Main";
+
+const App: React.FC<any> = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/main" component={Main} />
+        {/* <Route path="/lists/:listId" component={} /> */}
+        <Redirect to="/main" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,5 @@
+const Main: React.FC<any> = () => {
+  return <>Main</>;
+};
+
+export default Main;
