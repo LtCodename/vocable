08985b40f160c32ab80a1ccae0cc77ff3cca695f
Yevhen Chernenko <yevhen.dev@alp.com>
2021-07-07 11:46:54 +0300

    getting data from a firebase collection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import React, { useEffect } from "react";
 import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
 import Main from "./components/Main/Main";
+import fire from "./Firebase";
 
 const App: React.FC<any> = () => {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = (): void => {
+    fire
+      .firestore()
+      .collection("users")
+      .get()
+      .then((snapshot) => {
+        // snapshot.forEach((doc) => {
+        //   let data = doc.data();
+        //   console.log(data);
+        // });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return (
     <BrowserRouter>
       <Switch>
